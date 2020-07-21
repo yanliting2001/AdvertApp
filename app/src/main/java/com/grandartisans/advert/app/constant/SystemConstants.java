85b1ea088612bf7c45b09dff45df4286e71a10cc
yanliting2001 <939757170@qq.com>
2020-07-21 20:19:30 +0800

    send key to project use serialport

--- app/src/main/java/com/grandartisans/advert/app/constant/SystemConstants.java
+++ app/src/main/java/com/grandartisans/advert/app/constant/SystemConstants.java
@@ -1,4 +1,11 @@
 package com.grandartisans.advert.app.constant;
 
 public interface SystemConstants {
+    boolean TFMIN_MOUDLE_ENABLE = true;
+    String TFMIN_MOUDLE_NAME = "/dev/ttymxc0";
+    int TFMIN_MOUDLE_BOUDRATE=115200;
+
+    boolean PROJECT_MOUDLE_ENABLE = true;
+    String PROJECT_MOUDLE_NAME = "/dev/ttymxc2";
+    int PROJECT_MOUDLE_BOUDRATE = 115200;
 }
--- app/src/main/java/com/grandartisans/advert/utils/ALFu700ProjectManager.java
+++ app/src/main/java/com/grandartisans/advert/utils/ALFu700ProjectManager.java
@@ -3,6 +3,7 @@ package com.grandartisans.advert.utils;
 import android.content.Context;
 import android.util.Log;
 
+import com.grandartisans.advert.app.constant.SystemConstants;
 import com.grandartisans.advert.interfaces.ElevatorDoorEventListener;
 
 public class ALFu700ProjectManager {
@@ -10,7 +11,9 @@ public class ALFu700ProjectManager {
     private volatile static ALFu700ProjectManager mProjectInstance = null;
     private SerialPortUtils serialPortUtils = null;
     private boolean isDeviceOpened  = false;
-    public ALFu700ProjectManager(Context context){}
+    public ALFu700ProjectManager(Context context){
+        openProject();
+    }
     public static ALFu700ProjectManager getInstance(Context context) {
         if (mProjectInstance == null) {
             synchronized (ALFu700ProjectManager.class) {
@@ -25,9 +28,19 @@ public class ALFu700ProjectManager {
     public boolean openProject(){
         if(isDeviceOpened==false) {
             serialPortUtils = new SerialPortUtils();
+            serialPortUtils.setOnDataReceiveListener(new SerialPortUtils.OnDataReceiveListener() {
+                @Override
+                public void onDataReceive(byte[] buffer, int size) {
+                    /*
+                    for(int i=0;i<size;i++) {
+                        Log.i(TAG,""+ (buffer[i]&0xff));
+                    }
+                    */
+                }
+            });
             if (serialPortUtils != null) {
-                if (serialPortUtils.openSerialPort("/dev/ttyS4") == null) {
-                    Log.i(TAG,"can not open device /dev/ttyS4");
+                if (serialPortUtils.openSerialPort(SystemConstants.PROJECT_MOUDLE_NAME,SystemConstants.PROJECT_MOUDLE_BOUDRATE) == null) {
+                    Log.i(TAG,"can not open device " + SystemConstants.PROJECT_MOUDLE_NAME);
                     return false;
                 }
                 isDeviceOpened = true;
@@ -45,12 +58,12 @@ public class ALFu700ProjectManager {
    public boolean SleepProject(){
        //关闭投影：AB 00 00 00 3B 00 00 00 00 00 00 00 00 00 00 10 00 00 00 F6
        //String hexData = "AB0000003B0000000000000000000010000000F6";
-       String hexData = "AB000000100000000000000000000000000000BB";
+       String hexData = "697620";
        return sendHexData(hexData);
    }
    public boolean WakeUpProject(){
        //开机：AF 00 00 00 10 01 00 00 00 00 00 00 00 00 00 00 00 00 00 C0
-       String hexData = "AF000000100100000000000000000000000000C0";
+       String hexData = "69771F";
        return sendHexData(hexData);
    }
    public boolean sWitchHdmi(){
@@ -75,22 +88,22 @@ public class ALFu700ProjectManager {
    }
     public boolean sendUpSlow(){
         //上：AB 00 00 00 3B 00 00 00 00 00 00 00 00 00 00 02 00 00 00 E8
-        String hexData = "AF0000023B0000000000000000000002000008F6";
+        String hexData = "69D7BF";
         return sendHexData(hexData);
     }
     public boolean sendDownSlow(){
         //下：AB 00 00 00 3B 00 00 00 00 00 00 00 00 00 00 08 00 00 00 EE
-        String hexData = "AF0000023B0000000000000000000008000008FC";
+        String hexData = "69C7CF";
         return sendHexData(hexData);
     }
     public boolean sendLeftSlow(){
         //左：AB 00 00 00 3B 00 00 00 00 00 00 00 00 00 40 00 00 00 00 26
-        String hexData = "AF0000023B000000000000000000400000000834";
+        String hexData = "69A7EF";
         return sendHexData(hexData);
     }
     public boolean sendRightSlow(){
         //右：AB 00 00 00 3B 00 00 00 00 00 00 00 00 00 00 40 00 00 00 26
-        String hexData = "AF0000023B000000000000000000004000000834";
+        String hexData = "69B7DF";
         return sendHexData(hexData);
     }
    public boolean sendUp(){
@@ -113,6 +126,23 @@ public class ALFu700ProjectManager {
        String hexData = "AB0000003B000000000000000000004000000026";
        return sendHexData(hexData);
    }
+   public boolean sendMenu(){
+        String hexData = "6997FF";
+       return sendHexData(hexData);
+   }
+    public boolean sendInput(){
+        String hexData = "697521";
+        return sendHexData(hexData);
+    }
+    public boolean sendExit(){
+        String hexData = "698412";
+        return sendHexData(hexData);
+    }
+
+    public boolean sendSelect(){
+        String hexData = "699600";
+        return sendHexData(hexData);
+    }
 
    private boolean sendHexData(String hexData){
        byte[] data =FileOperator.hexStr2Bytes(hexData);
